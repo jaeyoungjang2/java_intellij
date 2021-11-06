5ead0814ecd7cd7af9e024b3e355fca4bb4eae51
jaeyoungjang2 <lion85488@gmail.com>
2021-11-06 11:12:53 +0900

    java

---
+++ src/main/java/prepareSort2.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class baekjoon42746 {
+
+    public static void main(String[] args) {
+        String[] test = {"3", "30"};
+        int[] numbers = {6, 10, 2};
+    }
+}
