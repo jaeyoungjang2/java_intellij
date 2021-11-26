5a68298a52256ee6c4d77a586d11935f01d8963d
jaeyoungjang2 <lion85488@gmail.com>
2021-11-26 13:29:16 +0900

    java

--- src/main/java/prepareSort1.java
+++ src/main/java/prepareSort1.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class prepareSort1 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class prepareSort1 {
+
+    public static void main(String[] args) {
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+//        [5, 6, 3]
+
+        List<Integer> list = new ArrayList<>();
+        for (Integer number: array) {
+            list.add(number);
+        }
+
+
+        int[] answer = new int[3];
+        int index = 0;
+        for (int i = 0; i < commands.length; i++) {
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int target = commands[i][2];
+            List<Integer> subList = new ArrayList<>(list.subList(start - 1, end));
+            Collections.sort(subList);
+            answer[index] = subList.get(target - 1);
+            index ++;
+        }
+
+        System.out.println(Arrays.toString(answer));
+    }
 }
