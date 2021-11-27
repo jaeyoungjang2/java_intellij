bc058096e31f64c20f170678370d46f2b410c8e1
jaeyoungjang2 <lion85488@gmail.com>
2021-11-27 18:54:45 +0900

    자바

--- src/main/java/prepareSort3.java
+++ src/main/java/prepareSort3.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class prepareSort3 {
+import java.util.Arrays;
+import java.util.Collections;
+
+public class prepareSort3 {
+
+    public static void main(String[] args) {
+        int[] citations = {3, 0, 6, 1, 5};
+//        int[] citations = {350, 452, 877};
+        Arrays.sort(citations);
+
+        int answer = 0;
+        int numOfPaper = citations.length;
+        for (int i = 0; i <= numOfPaper; i++) {
+            int numOfCitation = citations[i];
+            if (numOfCitation >= numOfPaper) {
+                answer = numOfPaper ;
+                break;
+            }
+            numOfPaper--;
+        }
+        System.out.println(answer);
+    }
 }
