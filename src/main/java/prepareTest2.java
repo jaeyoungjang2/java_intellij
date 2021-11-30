63b59b90ed7e0e8d81b9bc3b363a7ee549fa81e5
jaeyoungjang2 <lion85488@gmail.com>
2021-11-30 21:29:27 +0900

    java

--- src/main/java/prepareTest2.java
+++ src/main/java/prepareTest2.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class prepareTest2 {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class prepareTest2 {
+
+    public static void main(String[] args) {
+        String[] phone_book = {"119", "97674223", "1195524421"};
+        Set<Integer> set = new HashSet<>();
+
+        boolean answer = true;
+        Map<String, Integer> map = new HashMap<>();
+
+
+        for (String phoneNumber : phone_book) {
+            map.put(phoneNumber, 0);
+            set.add(phoneNumber.length());
+        }
+
+        List<Integer> list = new ArrayList<>(set);
+        for (String phoneNumber : phone_book) {
+            if (!answer) {
+                break;
+            }
+            int phoneNumberLength = phoneNumber.length();
+            for (Integer length : list) {
+                if (length != phoneNumberLength &&  length < phoneNumberLength && map.containsKey(phoneNumber.substring(0, length))) {
+                    answer = false;
+                    break;
+                }
+            }
+        }
+    }
 }
