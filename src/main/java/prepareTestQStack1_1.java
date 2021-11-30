7e9c2596306b7ea1ae7c22bf046437d1173306ed
jaeyoungjang2 <lion85488@gmail.com>
2021-11-30 21:29:35 +0900

    java

--- src/main/java/prepareTestQStack1_1.java
+++ src/main/java/prepareTestQStack1_1.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class prepareTestQStack1_1 {
+import java.util.Arrays;
+
+public class prepareTestQStack1_1 {
+
+    public static void main(String[] args) {
+        int[] progresses = {99, 1, 99, 1};
+        int[] speeds = {1, 1, 1, 1};
+
+        int[] dayOfend = new int[100];
+        int day = -1;
+        for(int i=0; i<progresses.length; i++) {
+            while(progresses[i] + (day*speeds[i]) < 100) {
+                day++;
+            }
+            dayOfend[day]++;
+        }
+        System.out.println(Arrays.toString(dayOfend));
+        int[] ints = Arrays.stream(dayOfend).filter(i -> i != 0).toArray();
+        System.out.println(Arrays.toString(ints));
+    }
 }
