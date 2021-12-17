6a6bd6de77df25e33967f6482ae6506f2adf02e9
jaeyoungjang2 <lion85488@gmail.com>
2021-12-17 21:38:44 +0900

    java

--- src/main/java/BaseBallTest.java
+++ src/main/java/BaseBallTest.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class BaseBallTest {
+public class BaseBallTest {
+    private String a;
+
+    public static void main(String[] args) {
+        BaseBallTest2 baseBallTest2 = new BaseBallTest2();
+        baseBallTest2.test2();
+
+    }
 }
