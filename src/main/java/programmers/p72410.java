5c20897744362ef8223cab656a6590f914f6ea99
jaeyoungjang2 <lion85488@gmail.com>
2022-03-14 09:16:15 +0900

    java

---
+++ src/main/java/programmers/p72410.java
@@ -0,0 +1,2 @@
+package programmers;public class p72410 {
+}
---
+++ src/main/java/programmers/p87389.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class p87389 {
+
+    public static void main(String[] args) {
+        int n = 12;
+
+        // 시작
+        int answer = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 1) {
+                answer = i;
+                break;
+            }
+        }
+
+        System.out.println(answer);
+//        return answer;
+    }
+}
