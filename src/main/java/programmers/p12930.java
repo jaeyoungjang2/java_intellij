d2fb464a4fdc1075d4083ac3a46b78f0a3025163
jaeyoungjang2 <lion85488@gmail.com>
2022-03-17 22:24:33 +0900

    java

--- src/main/java/programmers/p12930.java
+++ src/main/java/programmers/p12930.java
@@ -1,2 +1,31 @@
-package programmers;public class p12930 {
+package programmers;
+
+
+
+public class p12930 {
+
+    public static void main(String[] args) {
+
+        String s = " aba  ";
+
+        String answer = "";
+
+        int index = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                answer += " ";
+                index = 0;
+                continue;
+            }
+            if (index % 2 == 0) {
+                answer += Character.toUpperCase(s.charAt(i));
+            }
+            if (index % 2 == 0) {
+                answer += Character.toLowerCase(s.charAt(i));
+            }
+        }
+
+        System.out.println(answer);
+//    return answer;
+    }
 }
