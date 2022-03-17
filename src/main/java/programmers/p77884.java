74aeff1b2d5b15d791107c2922e7c0c1a213d30f
jaeyoungjang2 <lion85488@gmail.com>
2022-03-18 06:27:25 +0900

    java

--- src/main/java/programmers/p77884.java
+++ src/main/java/programmers/p77884.java
@@ -1,2 +1,34 @@
-package programmers;public class p77884 {
+package programmers;
+
+public class p77884 {
+
+    public static void main(String[] args) {
+        int left = 24;
+        int right = 27;
+
+        // 시작
+        int answer = 0;
+
+        for (int i = left; i <= right; i++) {
+
+            int countMeasure = calCountMeasure(i);
+            if (countMeasure % 2 == 0) {
+                answer += i;
+                continue;
+            }
+            answer -= i;
+        }
+
+        // return answer;
+    }
+
+    private static int calCountMeasure(int i) {
+        int countMeasure = 0;
+        for (int j = 1; j <= i; j++) {
+            if (i % j == 0) {
+                countMeasure++;
+            }
+        }
+        return countMeasure;
+    }
 }
