556ee4915f315158cca100f5faa405b5ddb103e1
jaeyoungjang2 <lion85488@gmail.com>
2022-03-18 06:27:12 +0900

    java

---
+++ src/main/java/kakao60058.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class kakao60058 {
+}
--- src/main/java/programmers/p12935.java
+++ src/main/java/programmers/p12935.java
@@ -1,2 +1,35 @@
-package programmers;public class p12935 {
+package programmers;
+
+import java.util.Arrays;
+
+public class p12935 {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 2, 1};
+
+
+        int[] answer;
+
+        if (arr.length == 1) {
+            answer = new int[] {-1};
+//            return answer;
+        }
+
+        answer = new int[arr.length - 1];
+        int index = 0;
+        int minimumNum = Arrays.stream(arr).min().getAsInt();
+        for (int num: arr) {
+            System.out.println("num = " + num);
+            if (num == minimumNum) {
+                continue;
+            }
+            answer[index] = num;
+            index++;
+        }
+
+        for (int i : answer) {
+            System.out.println("i = " + i);
+        }
+//        return answer;
+    }
 }
