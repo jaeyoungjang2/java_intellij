632fff47817ecac0b6b6b12dcf9540e805b54fba
jaeyoungjang2 <lion85488@gmail.com>
2022-03-19 12:54:57 +0900

    java

--- src/main/java/programmers/p12932.java
+++ src/main/java/programmers/p12932.java
@@ -1,2 +1,22 @@
-package programmers;public class p12932 {
+package programmers;
+
+import java.util.Arrays;
+
+public class p12932 {
+
+    public static void main(String[] args) {
+        long n = 100000000000L;
+        System.out.println(n);
+        int[] answer = new int[(int)(Math.log10(n)+1)];
+        int index = 0;
+        
+        while (n != 0) {
+            answer[index] = (int) (n % 10);
+            index ++;
+            n /= 10;
+        }
+
+
+//        return answer;
+    }
 }
