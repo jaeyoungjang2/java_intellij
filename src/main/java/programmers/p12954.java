032c546c8a4180ae3fb57505330fca66a217e47f
jaeyoungjang2 <lion85488@gmail.com>
2022-03-19 12:55:06 +0900

    java

--- src/main/java/programmers/p12954.java
+++ src/main/java/programmers/p12954.java
@@ -1,2 +1,20 @@
-package programmers;public class p12954 {
+package programmers;
+
+import java.util.ArrayList;
+
+public class p12954 {
+
+    public static void main(String[] args) {
+        int x = 10000000;
+        int n = 200;
+
+        long[] answer = new long[n];
+        answer[0] = x;
+
+        for (int i = 1; i < n; i++) {
+            answer[i] += answer[0] * (i + 1);
+        }
+
+//        return answer;
+    }
 }
