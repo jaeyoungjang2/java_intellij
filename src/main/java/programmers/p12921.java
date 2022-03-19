487803ac9e9b1e78c949d5136bdb6a7e2b4455ca
jaeyoungjang2 <lion85488@gmail.com>
2022-03-19 12:55:14 +0900

    java

--- src/main/java/programmers/p12921.java
+++ src/main/java/programmers/p12921.java
@@ -1,2 +1,27 @@
-package programmers;public class p12921 {
+package programmers;
+
+public class p12921 {
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        // 시작
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isDecimal(i)) {
+                answer ++;
+            }
+        }
+
+//        return answer;
+    }
+
+    private static boolean isDecimal(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
