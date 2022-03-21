8e7f28d430517ae50214c4d835b4c02089032e85
jaeyoungjang2 <lion85488@gmail.com>
2022-03-21 21:55:45 +0900

    java

--- src/main/java/programmers/p12943.java
+++ src/main/java/programmers/p12943.java
@@ -1,2 +1,33 @@
-package programmers;public class p12943 {
+package programmers;
+
+public class p12943 {
+
+    public static void main(String[] args) {
+        long num = 626331;
+
+        int answer = 0;
+
+        int index = 0;
+        while (index <= 500 && num != 1) {
+            if (num % 2 == 0) {
+                num /= 2;
+                index ++;
+                continue;
+            }
+            num = (num * 3) + 1;
+            index ++;
+        }
+
+        if (num != 1) {
+
+            System.out.println(-1);
+            answer = -1;
+//            return answer;
+        }
+        answer = index;
+        System.out.println("index = " + index);
+        System.out.println("num = " + num);
+        System.out.println(answer);
+//        return answer;
+    }
 }
