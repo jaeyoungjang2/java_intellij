c646f2cf1de30f74b2feaa48c524232662a0ad3b
jaeyoungjang2 <lion85488@gmail.com>
2022-03-22 09:46:52 +0900

    java

--- src/main/java/programmers/p12917.java
+++ src/main/java/programmers/p12917.java
@@ -1,2 +1,38 @@
-package programmers;public class p12917 {
+package programmers;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class p12917 {
+
+    public static void main(String[] args) {
+
+        String s = "Zbcdefg";
+        String answer = "";
+
+        String upperCase = "";
+        String lowerCase = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isUpperCase(s.charAt(i))) {
+                upperCase += s.charAt(i);
+                continue;
+            }
+            lowerCase += s.charAt(i);
+        }
+
+
+        String[] lowerSplit = lowerCase.split("");
+        Arrays.sort(lowerSplit, Collections.reverseOrder());
+        String lower = String.join("", lowerSplit);
+
+        String[] upperSplit = upperCase.split("");
+        Arrays.sort(upperSplit, Collections.reverseOrder());
+        String upper = String.join("", upperSplit);
+
+        answer = lower + upper;
+        System.out.println(answer);
+//        return answer;
+    }
+
 }
