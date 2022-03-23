2df50f78f00a40d209e40e3a64116c40a6480de6
jaeyoungjang2 <lion85488@gmail.com>
2022-03-23 21:17:43 +0900

    .gitignore test

--- src/main/java/programmers/p12948.java
+++ src/main/java/programmers/p12948.java
@@ -1,2 +1,20 @@
-package programmers;public class p12948 {
+package programmers;
+
+public class p12948 {
+
+    public static void main(String[] args) {
+        String phone_number = "01033334444";
+        String answer = "";
+
+        String endNumber = phone_number.substring(phone_number.length() - 4, phone_number.length());
+
+        for (int i = 0; i < phone_number.length() - 4; i++) {
+            answer += "*";
+        }
+        answer += endNumber;
+
+
+        System.out.println(answer);
+//        return answer;
+    }
 }
