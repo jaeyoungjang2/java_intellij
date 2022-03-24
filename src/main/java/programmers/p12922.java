686a49eadaa9641e8dc0fd67627a0106a90cbc1a
jaeyoungjang2 <lion85488@gmail.com>
2022-03-24 10:13:23 +0900

    .gitignore test

--- src/main/java/programmers/p12922.java
+++ src/main/java/programmers/p12922.java
@@ -1,2 +1,22 @@
-package programmers;public class p12922 {
+package programmers;
+
+import java.util.Collections;
+import java.util.List;
+
+public class p12922 {
+
+    public static void main(String[] args) {
+        int n = 4;
+
+        String answer = "";
+        for (int i = 0; i < n/2; i++) {
+            answer += "수박";
+        }
+        if (n % 2 == 1) {
+            answer += "수";
+        }
+
+        System.out.println(answer);
+//        return answer;
+    }
 }
