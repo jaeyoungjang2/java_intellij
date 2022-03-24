2c96915df97a2b08082b006a918053afd4215552
jaeyoungjang2 <lion85488@gmail.com>
2022-03-24 10:13:35 +0900

    java

--- src/main/java/programmers/p12919.java
+++ src/main/java/programmers/p12919.java
@@ -1,2 +1,20 @@
-package programmers;public class p12919 {
+package programmers;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class p12919 {
+
+    public static void main(String[] args) {
+        String[] seoul = {"Jane", "Kim"};
+
+        String answer = "";
+
+        List<String> list = Arrays.asList(seoul);
+        int kimIndex = list.indexOf("Kim");
+
+        answer = String.format("김서방은 %s에 있다", kimIndex);
+//        return answer;
+
+    }
 }
