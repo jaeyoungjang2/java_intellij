3ed5a1adf7b98c4f797e5ae75d68e8961d95b7e2
jaeyoungjang2 <lion85488@gmail.com>
2022-03-24 10:13:44 +0900

    java

--- src/main/java/programmers/p12947.java
+++ src/main/java/programmers/p12947.java
@@ -1,2 +1,21 @@
-package programmers;public class p12947 {
+package programmers;
+
+import java.util.Arrays;
+
+public class p12947 {
+
+    public static void main(String[] args) {
+
+        int x = 13;
+
+        System.out.println(hashard(x));
+//        return hashard(x);
+    }
+
+    private static boolean hashard(int x) {
+        String[] split = String.valueOf(x).split("");
+        int total = Arrays.stream(split).mapToInt(Integer::parseInt).sum();
+        return x % total == 0;
+    }
+
 }
