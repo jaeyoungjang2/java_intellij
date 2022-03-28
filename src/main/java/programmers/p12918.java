354b82f69d700df53ebd63ae20dae354922fc3ad
jaeyoungjang2 <lion85488@gmail.com>
2022-03-29 06:14:03 +0900

    java

--- src/main/java/programmers/p12918.java
+++ src/main/java/programmers/p12918.java
@@ -1,2 +1,31 @@
-package programmers;public class p12918 {
+package programmers;
+
+public class p12918 {
+
+    public static void main(String[] args) {
+//        String s = "a234";
+
+//        false 이어야 하는데 지금 true로 나오고 있음
+        String s = "eeee";
+
+        boolean answer = false;
+
+        if (lengthCheck(s) && digitCheck(s)) {
+            answer = true;
+        }
+        System.out.println(answer);
+//        return answer;
+    }
+
+    private static boolean digitCheck(String s) {
+        return s.chars().allMatch(Character::isDigit);
+    }
+
+    private static boolean lengthCheck(String s) {
+
+        if (s.length() == 4 || s.length() == 6) {
+            return true;
+        }
+        return false;
+    }
 }
