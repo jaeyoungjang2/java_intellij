681f98ed2e925fc1e203407404a73e9d53837b4d
jaeyoungjang2 <lion85488@gmail.com>
2022-03-30 22:42:47 +0900

    java

--- src/main/java/programmers/p12931.java
+++ src/main/java/programmers/p12931.java
@@ -1,2 +1,20 @@
-package programmers;public class p12931 {
+package programmers;
+
+public class p12931 {
+
+    public static void main(String[] args) {
+
+        int n = 123;
+        int answer = 0;
+
+        String string = String.valueOf(n);
+        for (int i = 0; i < string.length(); i++) {
+            answer += Character.getNumericValue(string.charAt(i));
+        }
+
+        System.out.println(answer);
+//        return answer;
+
+
+    }
 }
