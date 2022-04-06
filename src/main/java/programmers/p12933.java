e443846eb4c4ed513814f3247f4df3f625c9d7c7
jaeyoungjang2 <lion85488@gmail.com>
2022-04-07 08:53:49 +0900

    java

--- src/main/java/programmers/p12933.java
+++ src/main/java/programmers/p12933.java
@@ -1,2 +1,31 @@
-package programmers;public class p12933 {
+package programmers;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+public class p12933 {
+
+    public static void main(String[] args) {
+        long n = 118372;
+        long answer = 0;
+
+        String s = String.valueOf(n);
+        String[] split = s.split("");
+        System.out.println("s = " + s);
+        
+        Arrays.sort(split, Collections.reverseOrder());
+
+        String temp = "";
+        for (int i = 0; i < split.length; i++) {
+            temp += split[i];
+        }
+
+        answer = Long.parseLong(temp);
+//        return answer;
+
+
+        
+        
+    }
 }
