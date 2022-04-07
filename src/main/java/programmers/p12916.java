68f1719b039c6dbfef025aec9e7d95742770631c
jaeyoungjang2 <lion85488@gmail.com>
2022-04-08 08:08:12 +0900

    java

--- src/main/java/programmers/p12916.java
+++ src/main/java/programmers/p12916.java
@@ -1,2 +1,31 @@
-package programmers;public class p12916 {
+package programmers;
+
+public class p12916 {
+
+    public static void main(String[] args) {
+        String s = "pyY";
+        boolean answer = true;
+
+
+        int pSize = 0;
+        int ySize = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'p' || c == 'P') {
+                pSize += 1;
+                continue;
+            }
+            if (c == 'y' || c == 'Y') {
+                ySize += 1;
+                continue;
+            }
+        }
+
+        if (pSize != ySize) {
+            answer = false;
+        }
+
+        System.out.println(answer);
+//        return answer;
+    }
 }
