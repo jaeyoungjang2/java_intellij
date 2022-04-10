bc29c76439179ec703a982e2749c465fdc85fa50
jaeyoungjang2 <lion85488@gmail.com>
2022-04-10 19:40:32 +0900

    java

--- src/main/java/programmers/p12934.java
+++ src/main/java/programmers/p12934.java
@@ -1,2 +1,20 @@
-package programmers;public class p12934 {
+package programmers;
+
+public class p12934 {
+
+    public static void main(String[] args) {
+        long n = 121;
+
+        long answer = 0;
+
+        double sqrt = Math.sqrt(n);
+        System.out.println("sqrt = " + sqrt);
+
+        if (Math.ceil(sqrt) != Math.floor(sqrt)) {
+            answer = -1;
+        } else {
+            answer = (long) Math.pow(Math.ceil(sqrt) + 1, 2);
+        }
+//        return answer;
+    }
 }
