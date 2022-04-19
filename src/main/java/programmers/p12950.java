359b955585bbb1bc9dace2fbb7fdc3aea7206ecd
jaeyoungjang2 <lion85488@gmail.com>
2022-04-20 08:33:17 +0900

    java

--- src/main/java/programmers/p12950.java
+++ src/main/java/programmers/p12950.java
@@ -1,2 +1,20 @@
-package programmers;public class p12950 {
+package programmers;
+
+public class p12950 {
+
+    public static void main(String[] args) {
+        int[][] arr1 = {{1, 2}, {2, 3}};
+        int[][] arr2 = {{3, 4}, {5, 6}};
+        int[][] answer = new int[arr1.length][arr1[0].length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+
+        }
+
+        System.out.println("answer = " + answer);
+//        return answer;
+    }
 }
