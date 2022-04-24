2a7dd88289ba63d80815dee445b75edc93f86141
jaeyoungjang2 <lion85488@gmail.com>
2022-04-24 13:43:18 +0900

    java

--- src/main/java/pTest.java
+++ src/main/java/pTest.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class pTest {
+public class pTest {
+
+    public static void main(String[] args) {
+        String name = "AAAABBBBBBA";
+        int answer = 0;
+        int[] diff={0,1,2,3,4,5,6,7,8,9,10,11,12,13,12,11,10,9,8,7,6,5,4,3,2,1};
+        for(char c:name.toCharArray())
+            answer+=diff[c-'A'];
+
+        int length=name.length();
+        int min=length-1;
+
+        for(int i=0;i<length;i++){
+            int next=i+1;
+            while(next<length && name.charAt(next)=='A'){
+                next++;
+            }
+            min=Math.min(min,i+length-next+Math.min(i,length-next));
+        }
+
+        System.out.println(answer + min);
+//        return answer+min;
+    }
 }
